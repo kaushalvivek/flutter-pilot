563cb40fe8074ddedf13b33b555ac1c0e5cbe9fc
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2019-06-11 18:48:05 +0800

    Partial Section 5

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# flutter-pilot
+# Flutter Pilot
+
 A pilot project in Flutter, to get a hold of the basics.
+
+This project follows the coursework available on Udemy at:
+[https://www.udemy.com/learn-flutter-dart-to-build-ios-android-apps/](https://www.udemy.com/learn-flutter-dart-to-build-ios-android-apps/)
--- flutter_pilot/lib/products.dart
+++ flutter_pilot/lib/products.dart
@@ -5,19 +5,38 @@ class Products extends StatelessWidget {
   Products(this.products);
 
   @override
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpeg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Column(
-        children: products
-            .map(
-              (element) => Card(
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset('assets/food.jpeg'),
-                        Text(element)
-                      ],
-                    ),
-                  ),
-            )
-            .toList());
+    /*
+      Column is not a great widgent to use
+      when you know that your code is going
+      to spill over.
+
+      A suitable alternative to Column is ListView.
+
+      ListView requires a unique container with
+      specified height.
+
+      It's expensive and inefficient  to create 
+      ListView for large # of elements, and doesn't
+      make sense when you aren't even gonna display
+      all of the content at one point of time.
+
+      A suitable specification is ListView.builder
+    */
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
   }
 }
